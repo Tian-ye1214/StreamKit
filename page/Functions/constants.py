5d8ec97059b3be10a50738aa44e4d5306beae270
天烨 <83327454+Tian-ye1214@users.noreply.github.com>
2025-02-14 17:24:15 +0800

    添加免费模型映射
    添加免费模型映射

--- page/Functions/constants.py
+++ page/Functions/constants.py
@@ -1,4 +1,4 @@
-# 新建常量文件存放模型映射
+# 付费模型
 MODEL_MAPPING = {
     "DeepSeek-v3": "Pro/deepseek-ai/DeepSeek-V3",
     "DeepSeek-R1": "Pro/deepseek-ai/DeepSeek-R1",
@@ -10,6 +10,20 @@ MODEL_MAPPING = {
     "Qwen2.5-72B": "Qwen/Qwen2.5-72B-Instruct-128K",
     "QVQ(支持多模态)": "Qwen/QVQ-72B-Preview",
     "Qwen2-VL-72B(支持多模态)": "Qwen/Qwen2-VL-72B-Instruct",
+}
+
+# 免费模型
+FREE_MODEL_MAPPING = {
+    "R1-Distill-Qwen-1.5B": "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
+    "R1-Distill-Qwen-7B": "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
+    "R1-Distill-Llama-8B": "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
+    "Qwen2.5-7B": "Qwen/Qwen2.5-7B-Instruct",
+    "Qwen2.5-Coder-7B": "Qwen/Qwen2.5-Coder-7B-Instruct",
+    "InternLM2.5-7B": "internlm/internlm2_5-7b-chat",
+    "Llama-3.1-8B": "meta-llama/Meta-Llama-3.1-8B-Instruct",
+    "GLM-4-9B": "THUDM/glm-4-9b-chat",
+    "Yi-1.5-9B": "01-ai/Yi-1.5-9B-Chat-16K",
+    "Gemma2-9b": "google/gemma-2-9b-it",
 }
 
 MULTIMODAL_MODELS = [
