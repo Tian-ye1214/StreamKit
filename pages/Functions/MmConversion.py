3de791d7509784088e5d065a43ee587e7a2ab3f0
YAN YUNXI <91200382+yanyunxi@users.noreply.github.com>
2025-02-15 18:02:31 +0800

    完善janus推理

--- pages/Functions/MmConversion.py
+++ pages/Functions/MmConversion.py
@@ -4,47 +4,53 @@ from transformers import AutoModelForCausalLM
 from janus.models import MultiModalityCausalLM, VLChatProcessor
 from janus.utils.io import load_pil_images
 
-# specify the path to the model
-model_path = "deepseek-ai/Janus-Pro-1B"
-vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
-tokenizer = vl_chat_processor.tokenizer
+def mmconversion(model_path, image, question):
+    # specify the path to the model
+    vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
+    tokenizer = vl_chat_processor.tokenizer
 
-vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(
-    model_path, trust_remote_code=True
-)
-vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+    vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(
+        model_path, trust_remote_code=True
+    )
+    vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
 
-question  = ""
 
-conversation = [
-    {
-        "role": "<|User|>",
-        "content": "<image_placeholder>\nExtract all information from this image and convert them into markdown format.",
-        "images": ["/home/li/桌面/山水画训练数据集/白雪石1_1/005 白雪石_225.jpg"],
-    },
-    {"role": "<|Assistant|>", "content": ""},
-]
+    conversation = [
+        {
+            "role": "<|User|>",
+            "content": f"<image_placeholder>\n{question}",
+            "images": [image],
+        },
+        {"role": "<|Assistant|>", "content": ""},
+    ]
 
-# load images and prepare for inputs
-pil_images = load_pil_images(conversation)
-prepare_inputs = vl_chat_processor(
-    conversations=conversation, images=pil_images, force_batchify=True
-).to(vl_gpt.device)
+    # load images and prepare for inputs
+    pil_images = load_pil_images(conversation)
+    prepare_inputs = vl_chat_processor(
+        conversations=conversation, images=pil_images, force_batchify=True
+    ).to(vl_gpt.device)
 
-# # run image encoder to get the image embeddings
-inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+    # # run image encoder to get the image embeddings
+    inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
 
-# # run the model to get the response
-outputs = vl_gpt.language_model.generate(
-    inputs_embeds=inputs_embeds,
-    attention_mask=prepare_inputs.attention_mask,
-    pad_token_id=tokenizer.eos_token_id,
-    bos_token_id=tokenizer.bos_token_id,
-    eos_token_id=tokenizer.eos_token_id,
-    max_new_tokens=512,
-    do_sample=False,
-    use_cache=True,
-)
+    # # run the model to get the response
+    outputs = vl_gpt.language_model.generate(
+        inputs_embeds=inputs_embeds,
+        attention_mask=prepare_inputs.attention_mask,
+        pad_token_id=tokenizer.eos_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+        max_new_tokens=512,
+        do_sample=False,
+        use_cache=True,
+    )
 
-answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
-print(f"{prepare_inputs['sft_format'][0]}", answer)
+    answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+    # results = f"{prepare_inputs['sft_format'][0]}" + answer
+    return answer
+
+# model_path = "deepseek-ai/Janus-Pro-1B"
+# question = "图片里描述了什么"
+# image = "/home/li/桌面/山水画训练数据集/白雪石1_1/001 白雪石_1.jpg"
+# res = mmconversion(model_path,image,question)
+# print(res)
