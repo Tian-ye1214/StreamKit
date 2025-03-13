f0270b482f5e1be571e97a9df5b463e24fce40cf
天烨 <83327454+Tian-ye1214@users.noreply.github.com>
2025-03-13 23:22:10 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,19 @@
-# -*- coding: utf-8 -*-
 import streamlit as st
-from pages.Functions.BackendInteraction import BackendInteractionLogic
-from pages.Functions.Constants import (
-    MODEL_MAPPING,
-    REASON_MODELS,
-)
 
-st.set_page_config(
-    page_title="Chat With AI",
-    page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+pages = {
+    "AI交互平台": [
+        st.Page("navigation/Chat.py", title="AI对话平台"),
+        st.Page("navigation/MultimodalChat.py", title="多模态AI交互平台"),
+    ],
+    "Dify": [
+        st.Page("navigation/Dify.py", title="小红书文案生成"),
+    ],
+    "Tools": [
+        st.Page("navigation/LightRAG.py", title="知识图谱检索"),
+        st.Page("navigation/PDFTranslator.py", title="PDF翻译"),
+        st.Page("navigation/SAM_Segmentor.py", title="分割万物"),
+    ],
+}
 
-
-def main():
-    backend = BackendInteractionLogic()
-    backend.initialize_session_state()
-    st.markdown("""
-    <h1 style='text-align: center;'>
-        Chat With AI
-    </h1>
-    <div style='text-align: center; margin-bottom: 20px;'>
-    </div>
-    """, unsafe_allow_html=True)
-
-    with st.sidebar:
-        backend.user_interaction()
-        backend.start_new_conversation()
-
-        st.markdown("""
-        <h3 style='text-align: center;'>
-            模型配置
-        </h3>
-        """, unsafe_allow_html=True)
-        model_display = st.selectbox("选择模型", list(MODEL_MAPPING.keys()), index=1, help="选择模型")
-        st.session_state.model = MODEL_MAPPING[model_display]
-
-        if st.session_state.model not in REASON_MODELS:
-            st.session_state.system_prompt = "You are a helpful assistant."
-
-        backend.parameter_configuration()
-
-    for message in st.session_state.chat_messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    if prompt := st.chat_input("在这里输入您的问题："):
-        backend.user_input(prompt)
-        backend.search_interaction()
-
-        # AI响应
-        with st.chat_message("assistant"):
-            try:
-                backend.ai_generation()
-            except Exception as e:
-                st.error(f"生成回答时出错: {str(e)}")
-
-
-if __name__ == "__main__":
-    main()
+pg = st.navigation(pages)
+pg.run()
