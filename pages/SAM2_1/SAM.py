a60a329f03e8c0cadb618574195591950e4b6efe
天烨 <83327454+Tian-ye1214@users.noreply.github.com>
2025-03-13 20:39:07 +0800

    Update SAM.py

--- pages/SAM2_1/SAM.py
+++ pages/SAM2_1/SAM.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
-from sam2.build_sam import build_sam2
-from sam2.sam2_image_predictor import SAM2ImagePredictor
+from pages.SAM2_1.sam2.build_sam import build_sam2
+from pages.SAM2_1.sam2.sam2_image_predictor import SAM2ImagePredictor
 import cv2
 
 class SAM2Segment:
