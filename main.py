1a8fead604b9266b2d0725cbd40d8e62ec5527d3
YAN YUNXI <91200382+yanyunxi@users.noreply.github.com>
2025-03-21 23:08:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ pages = {
     "AI交互平台": [
         st.Page("navigation/Chat.py", title="AI对话平台"),
         st.Page("navigation/MultimodalChat.py", title="多模态AI交互平台"),
+        st.Page("navigation/ImageGenerator.py", title="文本生成图像"),
     ],
     "Dify": [
         st.Page("navigation/Dify.py", title="小红书文案生成"),
     ],
     "Tools": [
         st.Page("navigation/LightRAG.py", title="知识图谱检索"),
-        st.Page("navigation/PDFTranslator.py", title="PDF翻译"),
+        st.Page("navigation/PDFTranslator.py", title="PDF固版翻译"),
         st.Page("navigation/SAM_Segmentor.py", title="分割万物"),
     ],
 }
