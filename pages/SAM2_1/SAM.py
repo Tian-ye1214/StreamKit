b88f41a71d21b8e922a50b3b2d629260d874beb1
天烨 <83327454+Tian-ye1214@users.noreply.github.com>
2025-03-26 16:21:05 +0800

    Update Box Inference & Auto Masks Inference

--- pages/SAM2_1/SAM.py
+++ pages/SAM2_1/SAM.py
@@ -5,6 +5,7 @@ from pages.SAM2_1.sam2.sam2_image_predictor import SAM2ImagePredictor
 from pages.SAM2_1.sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
 import cv2
 
+
 class SAM2Segment:
     def __init__(self, sam2_checkpoint, model_cfg):
         self.device = torch.device("cuda")
@@ -17,9 +18,9 @@ class SAM2Segment:
         self.mask_generator = SAM2AutomaticMaskGenerator(build_sam2(self.model_cfg, self.sam2_checkpoint
                                                                     , device=self.device, apply_postprocessing=False))
 
-    def show_mask(self, mask, color=None, image=None):
+    def show_mask(self, mask, color=None, image=None, box=None):
         if color is None:
-            color = np.array([30/255, 144/255, 255/255, 0.6])
+            color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
         h, w = mask.shape[-2:]
         mask = (mask > 127).astype(np.uint8)
 
@@ -29,14 +30,19 @@ class SAM2Segment:
             mask_colored = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
             mask_area = (mask > 0).reshape(h, w, 1)
             result = np.where(mask_area,
-                             result * (1 - color[3]) + mask_colored[:,:,:3] * 255 * color[3],
-                             result)
+                              result * (1 - color[3]) + mask_colored[:, :, :3] * 255 * color[3],
+                              result)
         else:
             result = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
 
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
         result = cv2.drawContours(result.astype(np.uint8), contours, -1, (255, 255, 255), thickness=2)
+        
+        if box is not None:
+            x0, y0 = int(box[0]), int(box[1])
+            x1, y1 = int(box[2]), int(box[3])
+            cv2.rectangle(result, (x0, y0), (x1, y1), (0, 0, 255), 2)
 
         return result
 
@@ -56,6 +62,28 @@ class SAM2Segment:
 
         return result
 
+    def show_masks(self, image, masks):
+        """生成带有随机颜色和轮廓的掩码叠加图"""
+        if len(masks) == 0:
+            return np.zeros_like(image)
+
+        sorted_masks = sorted(masks, key=lambda x: x['area'], reverse=True)
+        h, w = image.shape[:2]
+        overlay = np.zeros((h, w, 4), dtype=np.float32)
+
+        for ann in sorted_masks:
+            mask = ann['segmentation'].astype(np.uint8)
+            mask = (mask > 0).astype(np.uint8)
+            color = np.concatenate([np.random.random(3)[::-1], [0.8]])
+            overlay[mask.astype(bool)] = color
+
+            contours, _ = cv2.findContours(mask,
+                                           cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_NONE)
+            contours = [cv2.approxPolyDP(c, 0.01, closed=True) for c in contours]
+            cv2.drawContours(overlay, contours, -1, (0, 0, 1.0, 0.4), 1)
+        return (overlay * 255).astype(np.uint8)
+
     def point_inference(self, image, input_point=None, input_label=None):
         self.predictor.set_image(image)
         masks, scores, _ = self.predictor.predict(
@@ -67,29 +95,17 @@ class SAM2Segment:
         masks = masks[sorted_ind]
         return masks
 
+    def box_inference(self, image, input_box=None):
+        self.predictor.set_image(image)
+        masks, scores, _ = self.predictor.predict(
+            point_coords=None,
+            point_labels=None,
+            box=input_box,
+            multimask_output=False,
+        )
+        return masks
+
+
     def auto_mask_genarator(self, image):
         masks = self.mask_generator.generate(image)
         return masks
-
-    def show_masks(self, image, masks):
-        """生成带有随机颜色和轮廓的掩码叠加图"""
-        if len(masks) == 0:
-            return np.zeros_like(image)
-
-        sorted_masks = sorted(masks, key=lambda x: x['area'], reverse=True)
-        h, w = image.shape[:2]
-        overlay = np.zeros((h, w, 4), dtype=np.float32)
-        
-        for ann in sorted_masks:
-            mask = ann['segmentation'].astype(np.uint8)
-            mask = (mask > 0).astype(np.uint8) 
-            color = np.concatenate([np.random.random(3)[::-1], [0.8]])
-            overlay[mask.astype(bool)] = color
-
-            contours, _ = cv2.findContours(mask,
-                                         cv2.RETR_EXTERNAL,
-                                         cv2.CHAIN_APPROX_NONE)
-            contours = [cv2.approxPolyDP(c, 0.01, closed=True) for c in contours]
-            cv2.drawContours(overlay, contours, -1, (0, 0, 1.0, 0.4), 1)
-        return (overlay * 255).astype(np.uint8)
-
