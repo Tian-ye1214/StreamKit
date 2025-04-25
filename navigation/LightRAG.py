38f5084f6bc6cf0dae07e8637fff44978e6fca2b
天烨 <83327454+Tian-ye1214@users.noreply.github.com>
2025-04-25 23:27:27 +0800

    Update LightRAG.py

--- navigation/LightRAG.py
+++ navigation/LightRAG.py
@@ -341,7 +341,6 @@ def main():
 if 'previous_page' not in st.session_state:
     st.session_state.previous_page = 'lightRAG'
 current_page = 'lightRAG'
-print(current_page)
 if current_page != st.session_state.previous_page:
         st.session_state.clear()
         st.session_state.previous_page = current_page
