159aad5fda93c6b23dbe4f2c46f4ece297756dfc
天烨 <83327454+Tian-ye1214@users.noreply.github.com>
2025-06-10 21:06:14 +0800

    Update Constants.py

--- pages/Functions/Constants.py
+++ pages/Functions/Constants.py
@@ -2,42 +2,50 @@ HIGHSPEED_MODEL_MAPPING = {
     "DeepSeek-V3": "deepseek-chat",
     "DeepSeek-R1": "deepseek-reasoner",
     "通义千问3-235B": "qwen3-235b-a22b",
-    "通义千问3-30B": "qwen3-30b-a3b",
     "通义千问3-32B": "qwen3-32b",
+    "文心一言-4.5": "ernie-4.5-turbo-128k",
     "智谱清言-4": "glm-4-plus",
-    "豆包": "Doubao-pro-32k",
+    "豆包-1.5": "doubao-1.5-thinking-pro",
+    "Claude-4": "claude-sonnet-4-20250514",
+    "Claude-4-Thinking": "claude-sonnet-4-20250514-thinking",
     "ChatGPT-4o": "chatgpt-4o-latest",
-    "Grok-3-mini-fast": "grok-3-mini-fast-beta",
     "GPT-4.1": "gpt-4.1",
-    "Gemini-2.5-flash": "gemini-2.5-flash-preview-04-17",
-    "GPT-4.1-mini": "gpt-4.1-mini",
+    "Gemini-2.5-pro": "gemini-2.5-pro-preview-06-05",
+    "Gemini-2.5-pro-thinking": "gemini-2.5-pro-preview-06-05-thinking",
+    "Grok3": "grok-3",
 }
 
 VISIONMODAL_MAPPING = {
     "Qwen2.5-VL-72B": "qwen2.5-vl-72b-instruct",
-    "Qwen2.5-VL-7B": "qwen2.5-vl-7b-instruct",
-    "GPT-4.1-mini": "gpt-4.1-mini",
-    "Gemini-2.5-flash": "gemini-2.5-flash-preview-04-17",
+    "文心一言-4.5": "ernie-4.5-turbo-vl-32k",
+    "智谱清言-4": "glm-4v-plus",
+    "Gemini2.5-pro": "gemini-2.5-pro-preview-06-05",
+    "Gemini2.5-pro-thinking": "gemini-2.5-pro-preview-06-05-thinking",
     "ChatGPT-4o": "chatgpt-4o-latest",
     "GPT-4.1": "gpt-4.1",
-    "Claude3.7": "claude-3-7-sonnet-20250219",
+    "Claude-4": "claude-sonnet-4-20250514",
+    "Claude-4-Thinking": "claude-sonnet-4-20250514-thinking",
 }
 
 MAX_TOKEN_LIMIT = {
-    "deepseek-chat": 64000,
-    "deepseek-reasoner": 64000,
-    "qwen3-235b-a22b": 64000,
-    "qwen3-30b-a3b": 64000,
-    "qwen3-32b": 64000,
-    "glm-4-plus": 128000,
-    "Doubao-pro-32k": 32000,
-    "chatgpt-4o-latest": 128000,
-    "grok-3-mini-fast-beta": 200000,
-    "gpt-4.1": 128000,
-    "gpt-4.1-mini": 128000,
-    "qwen2.5-vl-72b-instruct": 16000,
-    "qwen2.5-vl-7b-instruct": 16000,
-    "claude-3-7-sonnet-20250219": 200000,
+    "deepseek-chat": 65536,
+    "deepseek-reasoner": 65536,
+    "ernie-4.5-turbo-vl-32k": 32768,
+    "ernie-4.5-turbo-128k": 131072,
+    "doubao-1.5-thinking-pro": 32768,
+    "gemini-2.5-pro-preview-06-05": 200000,
+    "gemini-2.5-pro-preview-06-05-thinking": 200000,
+    "LLaMa-4-Maverick": 32768,
+    "qwen3-235b-a22b": 32768,
+    "qwen3-32b": 32768,
+    "glm-4-plus": 32768,
+    "doubao-1.5-thinking-vision-pro": 32768,
+    "chatgpt-4o-latest": 131072,
+    "grok-3": 200000,
+    "gpt-4.1": 131072,
+    "qwen2.5-vl-72b-instruct": 16384,
+    "claude-sonnet-4-20250514": 200000,
+    "claude-sonnet-4-20250514-thinking": 200000,
 }
 
 EMBEDDING_MODEL_MAPPING = {
@@ -45,16 +53,6 @@ EMBEDDING_MODEL_MAPPING = {
     "BGE-large-中文": "BAAI/bge-large-zh-v1.5",
     "BGE-large-英文": "BAAI/bge-large-en-v1.5",
 }
-
-
-REASON_MODELS = [
-    "Pro/deepseek-ai/DeepSeek-R1",
-    "deepseek-ai/DeepSeek-R1-Distill-Llama-70B",
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B",
-    "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
-]
 
 SEARCH_METHODS = {
     "文本搜索": "text_search",
