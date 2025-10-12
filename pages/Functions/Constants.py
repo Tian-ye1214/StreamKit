0c610e34251737f2661dd0795a4873c2cbce3c77
Tian-ye1214 <1092228927@qq.com>
2025-10-12 22:36:05 +0800

    模型更新

--- pages/Functions/Constants.py
+++ pages/Functions/Constants.py
@@ -2,13 +2,11 @@ HIGHSPEED_MODEL_MAPPING = {
     "DeepSeek-V3": "deepseek-chat",
     "DeepSeek-R1": "deepseek-reasoner",
     "通义千问3-235B": "qwen3-235b-a22b",
-    "智谱GLM-4.5": "glm-4.5-x",
+    "智谱GLM-4.6": "glm-4.6",
     "文心一言-4.5": "ernie-4.5-turbo-128k",
     "豆包-1.6-thinking": "doubao-seed-1.6-thinking",
-    "混元-T1(腾讯元宝)": "hunyuan-t1-latest",
     "Kimi-K2": "kimi-k2-0711-preview",
-    "MiniMax-M1": "MiniMax-M1",
-    "Claude-4-Sonnet-Thinking": "claude-sonnet-4-20250514-thinking",
+    "Claude-4.5-Sonnet-Thinking": "claude-sonnet-4-5-20250929-thinking",
     "GPT-5": "gpt-5",
     "Gemini-2.5-pro-thinking": "gemini-2.5-pro-thinking",
     "Grok4": "grok-4",
@@ -16,29 +14,28 @@ HIGHSPEED_MODEL_MAPPING = {
 }
 
 VISIONMODAL_MAPPING = {
-    "Qwen2.5-VL-72B": "qwen2.5-vl-72b-instruct",
+    "Qwen3-VL-235B": "qwen3-vl-235b-a22b-thinking",
     "文心一言-4.5": "ernie-4.5-turbo-vl-32k",
     "智谱GLM-4.5": "glm-4.5v",
     "Gemini2.5-pro-thinking": "gemini-2.5-pro-thinking",
     "GPT-5": "gpt-5",
-    "Claude-4.1-Opus-Thinking": "claude-sonnet-4-20250514-thinking",
+    "Claude-4.5-Sonnet-Thinking": "claude-sonnet-4-5-20250929-thinking",
 }
 
 MAX_TOKEN_LIMIT = {
     "deepseek-chat": 65536,
     "deepseek-reasoner": 65536,
     "qwen3-235b-a22b": 32768,
-    "qwen2.5-vl-72b-instruct": 16384,
-    "glm-4.5-x": 32768,
+    "Qwen3-VL-235B": 32768,
+    "glm-4.6": 32768,
     "glm-4.5v": 32768,
     "ernie-4.5-turbo-vl-32k": 32768,
     "ernie-4.5-turbo-128k": 131072,
     "doubao-seed-1.6-thinking": 32768,
-    "hunyuan-t1-latest": 32768,
     "gemini-2.5-pro-thinking": 200000,
     "gpt-5": 131072,
     "grok-4": 200000,
-    "claude-sonnet-4-20250514-thinking": 200000,
+    "claude-sonnet-4-5-20250929-thinking": 200000,
     "llama-4-maverick-17b-128e-instruct": 200000,
 }
 
@@ -49,6 +46,7 @@ EMBEDDING_MODEL_MAPPING = {
     "BGE-M3": "Pro/BAAI/bge-m3",
     "BCE-Base": "netease-youdao/bce-embedding-base_v1",
 }
+
 EMBEDDING_DIM = {
     "Qwen3-Embedding-8B": 4096,
     "Qwen3-Embedding-4B": 2048,
